6d9ef0e4f12cf87e388f49bd315057398f5897c4
dimligent <16304566+dimligent@user.noreply.gitee.com>
2025-11-12 21:17:46 +0800

    换头像

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -1,38 +1,38 @@
 import { defineConfig } from 'vitepress-theme-async/config';
 
 export default defineConfig({
-	srcDir: './',
-	themeConfig: {
-		topBars: [
-			{ title: 'Home', url: '/' },
-			{ title: 'About', url: '/about' },
-		],
-		links: [
-			{
-				name: 'Vitepress',
-				url: 'https://vitepress.dev',
-				image: 'https://vitepress.dev/vitepress-logo-mini.svg',
-				desc: 'VitePress is a Static Site Generator (SSG) designed for building fast, content-centric websites',
-			},
-			{
-				name: '白云苍狗',
-				url: 'https://www.imalun.com/',
-				image: 'https://www.imalun.com/images/avatar.jpg',
-				desc: '醒，亦在人间；梦，亦在人间',
-			},
-		],
-		rightside: {
-			readmode: true,
-			aside: true,
-		},
-		outline: {
-			level: [2, 6],
-		},
-		favicon: {
-			logo: '/favicon.svg',
-			icon16: '/favicon.svg',
-			icon32: '/favicon.svg',
-			visibilitychange: true,
-		},
-	},
+  srcDir: "./",
+  themeConfig: {
+    topBars: [
+      { title: "Home", url: "/" },
+      { title: "About", url: "/about" },
+    ],
+    links: [
+      {
+        name: "Vitepress",
+        url: "https://vitepress.dev",
+        image: "https://vitepress.dev/vitepress-logo-mini.svg",
+        desc: "VitePress is a Static Site Generator (SSG) designed for building fast, content-centric websites",
+      },
+      {
+        name: "chai的秘密基地",
+        url: "https://www.imalun.com/",
+        image: "D:\campus_life\photos\image_for_blog.jpg",
+        desc: "我记我记我记记记",
+      },
+    ],
+    rightside: {
+      readmode: true,
+      aside: true,
+    },
+    outline: {
+      level: [2, 6],
+    },
+    favicon: {
+      logo: "/favicon.svg",
+      icon16: "/favicon.svg",
+      icon32: "/favicon.svg",
+      visibilitychange: true,
+    },
+  },
 });
